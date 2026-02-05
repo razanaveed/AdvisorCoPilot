35e3a53edb411340da3779f2286d15fe9d574b29
Raza Naveed <raza.naveed@live.com>
2026-02-05 20:52:14 +0500

    changes

--- config/dev.exs
+++ config/dev.exs
@@ -55,12 +55,12 @@ config :advisor_co_pilot, AdvisorCoPilotWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/advisor_co_pilot_web/router\.ex$"E,
-      ~r"lib/advisor_co_pilot_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/advisor_co_pilot_web/router\.ex$",
+      ~r"lib/advisor_co_pilot_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
